b8b4d07c6c7c9f5c2ce75e9f057e3d6b027aaf2d
Hadjer <109548557+Hadjer-EL@users.noreply.github.com>
2023-07-11 23:09:55 +0100

    Create ThreadCode.java

---
+++ ThreadCode.java
@@ -0,0 +1,27 @@
+class ThreadCcode extends Thread{
+    public String c;
+     public  Semaphore s1 , s2;
+   public ThreadCode(String c , Semaphore s1, Semaphore s2){
+     this.c = c;
+     this.s1 = s1;
+     this.s2 = s2;
+     }
+    
+   public void run(){
+       try{
+           while(true){
+               s1.acquire();
+             System.out.println(this.c);
+               s2.release();
+               Thread.sleep(3000);
+             }
+       }
+       catch(Exception e){
+           e.printStackTrace();
+       }
+   
+     }
+
+
+
+}
