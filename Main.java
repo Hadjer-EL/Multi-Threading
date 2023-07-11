8e0102b643343e5feaa209402006e7f2a1dcfd9b
Hadjer <109548557+Hadjer-EL@users.noreply.github.com>
2023-07-11 23:10:38 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,9 +5,9 @@ public class Main {
    static Semaphore s3 = new Semaphore(0);
     public static void main(String[] args) {
       
-        Thread_code t1 = new Thread_code("a",s1,s2);
-        Thread_code t2 = new Thread_code("b",s2,s3);
-        Thread_code t3 = new Thread_code("c",s3,s1);
+        ThreadCode t1 = new Thread_code("a",s1,s2);
+        ThreadCode t2 = new Thread_code("b",s2,s3);
+        ThreadCode t3 = new Thread_code("c",s3,s1);
         
         t1.start();
         t2.start();
